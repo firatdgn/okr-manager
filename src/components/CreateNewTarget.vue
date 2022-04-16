2e8e71a1d1349d1e9857143d5ea75f291b803bba
Fırat Doğan <fratdogan@yandex.com>
2022-04-16 13:26:00 +0300

    feat: create new target component

---
+++ src/components/CreateNewTarget.vue
@@ -0,0 +1,96 @@
+<template>
+    <div
+        class="
+            is-flex is-justify-content-space-between is-align-items-baseline
+            key-result
+        "
+    >
+        <div
+            class="box is-flex-grow-3 is-flex is-justify-content-space-between"
+        >
+            <div
+                class="
+                    target-number
+                    is-flex is-justify-content-center is-align-items-center
+                "
+            >
+                {{ type.content }} - {{ number }}
+            </div>
+            <div class="target-content is-flex-grow-3">
+                <input class="input" type="text" />
+            </div>
+            <div
+                class="
+                    is-flex is-justify-content-center is-align-items-center
+                    pr-3
+                "
+            >
+                <span class="icon is-medium">
+                    <i class="fa-solid fa-lg fa-check"></i>
+                </span>
+                <span
+                    class="icon is-medium is-clickable"
+                    @click="$emit('cancel')"
+                >
+                    <i class="fa-solid fa-lg fa-xmark"></i>
+                </span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { ref } from "@vue/reactivity";
+export default {
+    props: ["type", "number"],
+    setup(props) {
+        let types = {
+            objective: {
+                color: "#ff8a00",
+                content: "O",
+            },
+            keyResult: {
+                color: "#5ae1c3",
+                content: "KR",
+            },
+            bhag: {
+                color: "#ff0000",
+                content: "BHAG",
+            },
+        };
+        let type = ref(types[props.type]);
+
+        return {
+            type,
+        };
+    },
+};
+</script>
+
+<style scoped>
+.box {
+    margin-top: 2.7%;
+    margin-left: 2.7%;
+    margin-right: 2.7%;
+    padding: 0;
+    font-weight: 600;
+    font-size: 18px;
+    --target-number-color: v-bind("type.color");
+}
+.target-number {
+    border-right: solid 1px #f7f8fa;
+    padding: 1.25em;
+    color: var(--target-number-color);
+    width: 10%;
+    text-align: center;
+}
+.target-content {
+    padding: 1.25em;
+}
+.fa-check {
+    color: #00d1b2;
+}
+.fa-xmark {
+    color: #ff4d4d;
+}
+</style>
