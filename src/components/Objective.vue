700673ea516f7e1563bc598ef7758042a5175ff6
Fırat Doğan <fratdogan@yandex.com>
2022-04-23 23:43:08 +0300

    style: key result deleting alert message typo

--- src/components/Objective.vue
+++ src/components/Objective.vue
@@ -131,7 +131,7 @@ export default {
         // Key Result
         function deleteKeyResult(deletedKeyResult) {
             console.log(deletedKeyResult.id);
-            if (confirm("Do you really want to delete this Objective?")) {
+            if (confirm("Do you really want to delete this Key Result?")) {
                 objective.value.keyResults = objective.value.keyResults.filter(
                     (elem) => deletedKeyResult.id !== elem.id
                 );
