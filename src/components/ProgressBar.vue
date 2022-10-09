951ced769d1102781c98345d0f52978c7c69fdc3
Fırat Doğan <fratdogan@yandex.com>
2022-10-09 18:11:38 +0300

    feat: progress bar component

---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <progress
+            class="progress is-success"
+            :value="currentStatus"
+            :max="finishedAt"
+        >
+            {{ currentStatus }}%
+        </progress>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["currentStatus", "finishedAt"],
+};
+</script>
+
+<style>
+</style>
